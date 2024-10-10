d6962fae0de2a0f0fe373f868547bca5e38fe705
Michael <bjmmtin@gmail.com>
2024-10-10 12:12:02 -0400

    add 'adding new WishItem'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,31 @@
 import { Component } from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  items : WishItem[] = [
+  items: WishItem[] = [
     new WishItem('TO learn Angular'),
     new WishItem('Get Coffe', true),
     new WishItem('Find Grass that cuts itself', true)
   ];
+  newWishtext = "";
   title = 'wishList';
 
-  toggleItem(item : WishItem){
+  toggleItem(item: WishItem) {
     item.isComplete = !item.isComplete;
+  }
+
+  addNewWish() {
+    //todo: add wish to items array
+    this.items.push(new WishItem(this.newWishtext));
+    this.newWishtext = '';
   }
 }
